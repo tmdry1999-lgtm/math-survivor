3dafb7dec0c928c26d19a7befc20b0c4f83a85c4
tmdry1999 <tmdry1999@gmaie.com>
2026-09-08 15:35:58 +0900

    feat: Phaser 부트스트랩과 placeholder 텍스처 추가

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log('math-survivor bootstrap ok');
+import Phaser from 'phaser';
+import { BootScene } from './scenes/BootScene.js';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'app',
+  backgroundColor: '#2b2b40',
+  physics: {
+    default: 'arcade',
+    arcade: { debug: false },
+  },
+  scene: [BootScene],
+};
+
+// eslint-disable-next-line no-new
+new Phaser.Game(config);
---
+++ src/scenes/BootScene.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+
+export class BootScene extends Phaser.Scene {
+  constructor() {
+    super('Boot');
+  }
+
+  create() {
+    this.createCircleTexture('player', 16, 0x4fd1c5);
+    this.createCircleTexture('enemy', 14, 0xf56565);
+    this.createCircleTexture('xpOrb', 6, 0xf6e05e);
+    this.scene.start('Stage', { unitId: 'nums_within_9' });
+  }
+
+  createCircleTexture(key, radius, color) {
+    const graphics = this.add.graphics();
+    graphics.fillStyle(color, 1);
+    graphics.fillCircle(radius, radius, radius);
+    graphics.generateTexture(key, radius * 2, radius * 2);
+    graphics.destroy();
+  }
+}
